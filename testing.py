49b7bcc6083b040323e64cf4d01f5e3677dc9201
Nallam.Karthikeya kumar <70764244+karthikeyakumar@users.noreply.github.com>
2020-11-16 20:35:05 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,64 @@
+from django.shortcuts import redirect, render
+import requests as d
+from .models import City
+from .forms import Cityform
+def index(request):
+    url="http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7779141bb1573e4ed78ef87a76a71f76"
+    err_msg=''
+    message=''
+    message_class=''
+    if request.method=='POST':
+        form=Cityform(request.POST)
+        if form.is_valid():
+            new_city=form.cleaned_data['name']
+            exist_c_count=City.objects.filter(name=new_city).count()
+            if exist_c_count==0:
+                r=d.get(url.format(new_city)).json()
+                if r['cod']==200:
+                    form.save()
+                else:
+                    err_msg="City doesn't exists in world" 
+            else:
+                err_msg='City already exists in Database !'
+    if err_msg:
+        message=err_msg
+        message_class='is-danger'
+    else:
+        message='city added sucesfully'
+        message_class='is-sucess'
+    
+    cities=City.objects.all()
+    ct=[]
+    form=Cityform()
+    for city in cities:
+        r=d.get(url.format(city)).json()
+        cityweather={
+            'city':r['name'],
+            'temperature':r['main']['temp'],
+            'humidity':r['main']['humidity'],
+            'icon':r['weather'][0]['icon'],
+            'description':r['weather'][0]['description']}
+        ct.append(cityweather)
+        
+
+    context={
+    'weatherdata':ct,
+    'form': form,
+    'message':message,
+    'message_class' : message_class
+    }
+
+    return render(request, 'weather.html',context)
+def delete_city(request,city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
+
+
+from django.forms import ModelForm,TextInput
+from django.forms import widgets
+from .models import City
+class Cityform(ModelForm):
+    class Meta:
+        model=City
+        fields=['name']
+        widgets={'name':TextInput(attrs={'class':'input','placeholder':'City Name'})}
